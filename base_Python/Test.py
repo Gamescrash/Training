4cb35e048e83b1fda854b91932f41dd14d8836da
Gamescrash <pallone2002@gmail.com>
2024-06-04 21:33:16 +0200

    Fine prima giornata

---
+++ base_Python/Test.py
@@ -0,0 +1,12 @@
+# BASE PYTHON (only for to remember something)
+
+#math ex
+    # import math
+
+    # print(math.pi)
+
+#file
+    # f=open("./testo.txt","a")
+    # # f.write("\nThis is a test n3")
+    # print(f.read())
+    # f.close()
